749c5c5223d217f2f43ff87d824e3315506ee0ad
prabwong <prabwong@gmail.com>
2023-04-30 03:01:24 +0700

    Detect_page

--- states/detect_state.py
+++ states/detect_state.py
@@ -7,20 +7,25 @@ class Detect_state(State):
         State.__init__(self, game)
         self.Dectect = Dectect(Dectect)
         self.time = 10
+        self.ori_time = 10
         self.Bg = Button("", "Lexend-VariableFont_wght.ttf", 20, (255, 255, 255), (255, 255, 255), (1600, 900), (0, 0))
         self.Table = Button("", "Lexend-VariableFont_wght.ttf", 20, (255, 255, 255), (255, 255, 255), (748, 561), (426, 170))
         # self.Text_1 = Button("Detecting players ...", "Amita-Regular.ttf", 64, (255, 255, 255), (255, 255, 255), (574, 124), (512, 381))
         self.Text_time = Button(f"Detecting players !", "Amita-Regular.ttf", 64, (255, 255, 255), (255, 255, 255), (574, 124), (512, 381))
+        self.arc = Button("", "Amita-Regular.ttf", 64, (255, 255, 255), (0, 0, 0), (127, 127), (736, 742))
+        self.Text_time1 = Button(f"{self.time}", "Amita-Regular.ttf", 48, (255, 255, 255), (0, 0, 0), (49, 93), (800, 806))
+
         
         self.num_people = int()
         self.start_count = True
+        self.circle = 270
         
         
     def update(self, delta_time, action):
         
         self.game.reset_keys() 
             
-        if self.time >= 0 and self.start_count == True:
+        if self.time > 0 and self.start_count == True:
             if (self.time) % 3 == 1:
                 self.Text_time = Button(f"Detecting players ...", "Amita-Regular.ttf", 64, (255, 255, 255), (255, 255, 255), (574, 124), (512, 381))
                 
@@ -31,20 +36,26 @@ class Detect_state(State):
                 self.Text_time = Button(f"Detecting players .", "Amita-Regular.ttf", 64, (255, 255, 255), (255, 255, 255), (574, 124), (512, 381))
             
             self.time -= 1
+            self.circle = self.circle - (360/self.ori_time)
+            self.Text_time1 = Button(f"{self.time}", "Amita-Regular.ttf", 48, (255, 255, 255), (0, 0, 0), (49, 93), (800, 806))
             time.sleep(1)
             
             
-        if self.time < 0 and self.start_count == True:
+        elif self.time == 0 and self.start_count == True:
             self.start_count = False
+            self.circle = 270
             self.time = 10
+            self.Text_time1 = Button(f"0", "Amita-Regular.ttf", 48, (255, 255, 255), (0, 0, 0), (49, 93), (800, 806))
             num_people = self.Dectect.count_people()
             self.game.num_player = num_people
             # print(self.game.num_player)
             print("Done count")
+            
+        elif self.start_count == False:
+            self.Text_time1 = Button(f"10", "Amita-Regular.ttf", 48, (255, 255, 255), (0, 0, 0), (49, 93), (800, 806))
+            self.start_count = True
             new_state = Confirm_people_state(self.game)
             new_state.enter_state()
-            self.start_count = True
-    
             
         
     def render(self, display):
@@ -52,9 +63,7 @@ class Detect_state(State):
         
         self.Bg.draw_image(display, 'Avalon_BG.png')
         self.Table.draw_image(display, 'Table.png')
-        
-        # self.Text_1.draw_text(display)
         self.Text_time.draw_text(display)
-        
-        
+        self.arc.draw_arc(display, -90, self.circle, 50, 4)
+        self.Text_time1.draw_text_center(display)
         

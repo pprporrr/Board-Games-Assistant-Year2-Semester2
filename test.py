cd295645d25c5ba748239e31107fe9f8e52e6abd
prabwong <prabwong@gmail.com>
2023-05-22 21:31:33 +0700

    test

--- test.py
+++ test.py
@@ -1,35 +1,22 @@
 import time
 from roboflow import Roboflow
-import cv2
+import cv2, os
 
 rf = Roboflow(api_key="szKo1b01Oo737AK9Apzk")
-project = rf.workspace().project("yolov5-avalon-scoretableau")
-model = project.version(1).model
+# project = rf.workspace().project("yolov5-avalon-scoretableau")
+project = rf.workspace().project("yolov5-avalon")
+model = project.version(4).model
 
-cap = cv2.VideoCapture(0)
+x=0
+camera = cv2.VideoCapture(1)
+while True:
+    ret, frame = camera.read()
+    cv2.imshow('frame', frame)
+    if x >= 150:
+        cv2.imwrite(os.path.join("C:\Prab\Year 2\Cognitive computer\Board-Games-Assistant-Year2-Semester2\capturePic", 'capturedPicture.jpg'), frame)
+        print(model.predict("C:\Prab\Year 2\Cognitive computer\Board-Games-Assistant-Year2-Semester2\capturePic\capturedPicture.jpg", confidence=40, overlap=30).json())
+        break
+    x+=1
 
-for i in range(5, 0, -1):
-    print(f"Capturing picture in {i} seconds...")
-    time.sleep(1)
-
-ret, frame = cap.read()
-
-if ret:
-    cv2.imshow('Virtual Webcam', frame)
-    cv2.imwrite('captured_picture.jpg', frame)
-    print(model.predict("/Users/ppr/Desktop/Project/Board-Games-Assistant-Year2-Semester2/IMG_6554.JPG", confidence=40, overlap=30).json())
-    print("\n")
-    print(model.predict("/Users/ppr/Desktop/Project/Board-Games-Assistant-Year2-Semester2/IMG_6555.JPG", confidence=40, overlap=30).json())
-    print("\n")
-    print(model.predict("/Users/ppr/Desktop/Project/Board-Games-Assistant-Year2-Semester2/IMG_6557.JPG", confidence=40, overlap=30).json())
-    print("\n")
-    print(model.predict("/Users/ppr/Desktop/Project/Board-Games-Assistant-Year2-Semester2/IMG_6558.JPG", confidence=40, overlap=30).json())
-    print("\n")
-    print(model.predict("/Users/ppr/Desktop/Project/Board-Games-Assistant-Year2-Semester2/IMG_6559.JPG", confidence=40, overlap=30).json())
-    print("\n")
-    print(model.predict("/Users/ppr/Desktop/Project/Board-Games-Assistant-Year2-Semester2/IMG_6561.JPG", confidence=40, overlap=30).json())
-else:
-    print("Failed to capture frame")
-
-cap.release()
+camera.release()
 cv2.destroyAllWindows()

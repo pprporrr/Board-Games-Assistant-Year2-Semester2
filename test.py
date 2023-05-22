9d91bee278b3b1e9ffe866073a9fdfb761aec4a4
ppr <pprporrr@gmail.com>
2023-05-22 15:29:31 +0700

    Test Webcam

--- test.py
+++ test.py
@@ -1,11 +1,35 @@
+import time
 from roboflow import Roboflow
+import cv2
 
 rf = Roboflow(api_key="szKo1b01Oo737AK9Apzk")
 project = rf.workspace().project("yolov5-avalon-scoretableau")
 model = project.version(1).model
 
-# infer on a local image
-print(model.predict("/Users/ppr/Desktop/Project/Board-Games-Assistant-Year2-Semester2/backup/Tableau10.JPG", confidence=40, overlap=30).json())
+cap = cv2.VideoCapture(0)
 
-# visualize your prediction
-model.predict("/Users/ppr/Desktop/Project/Board-Games-Assistant-Year2-Semester2/backup/Tableau10.JPG", confidence=40, overlap=30).save("Tableau10_prediction_2.JPG")
+for i in range(5, 0, -1):
+    print(f"Capturing picture in {i} seconds...")
+    time.sleep(1)
+
+ret, frame = cap.read()
+
+if ret:
+    cv2.imshow('Virtual Webcam', frame)
+    cv2.imwrite('captured_picture.jpg', frame)
+    print(model.predict("/Users/ppr/Desktop/Project/Board-Games-Assistant-Year2-Semester2/IMG_6554.JPG", confidence=40, overlap=30).json())
+    print("\n")
+    print(model.predict("/Users/ppr/Desktop/Project/Board-Games-Assistant-Year2-Semester2/IMG_6555.JPG", confidence=40, overlap=30).json())
+    print("\n")
+    print(model.predict("/Users/ppr/Desktop/Project/Board-Games-Assistant-Year2-Semester2/IMG_6557.JPG", confidence=40, overlap=30).json())
+    print("\n")
+    print(model.predict("/Users/ppr/Desktop/Project/Board-Games-Assistant-Year2-Semester2/IMG_6558.JPG", confidence=40, overlap=30).json())
+    print("\n")
+    print(model.predict("/Users/ppr/Desktop/Project/Board-Games-Assistant-Year2-Semester2/IMG_6559.JPG", confidence=40, overlap=30).json())
+    print("\n")
+    print(model.predict("/Users/ppr/Desktop/Project/Board-Games-Assistant-Year2-Semester2/IMG_6561.JPG", confidence=40, overlap=30).json())
+else:
+    print("Failed to capture frame")
+
+cap.release()
+cv2.destroyAllWindows()
